fbe411da21e61f8e78fbcd9690640e719819481e
FraneSpralja <spralja.frane@gmail.com>
2022-09-07 00:18:51 -0300

    Prueba Scripting para bloques expandibles

---
+++ assets/app.js
@@ -0,0 +1,73 @@
+// VARIABLES
+const historiaEmpresa = document.querySelector('.empresa_historia--parrafos')
+// EVENT LISTENER
+document.addEventListener('DOMContentLoaded', () => {
+    mostrarHistoriaDeLaEmpresa()
+});
+
+
+// FUNCTIONS
+function mostrarHistoriaDeLaEmpresa() {
+    const botonesExpandibles = document.querySelectorAll('.expandible_btn');
+
+    botonesExpandibles.forEach((btn) => {
+        btn.addEventListener('click', () => {
+            limpiarHTML()
+
+            console.log(btn.value)
+            const parrafoBreak = document.createElement('br')
+
+            const divExpandible = document.createElement('div');
+            divExpandible.classList.add('empresa_historia--expandible')
+
+            const divBody = document.createElement('div');
+            divBody.classList.add('expandible_body');
+
+            const parrafoExpandibleUno = document.createElement('p');
+            const parrafoExpandibleDos = document.createElement('p');
+            const parrafoExpandibleTres = document.createElement('p');
+            parrafoExpandibleUno.classList.add('expandible_body--text');
+            parrafoExpandibleDos.classList.add('expandible_body--text');
+            parrafoExpandibleTres.classList.add('expandible_body--text');
+
+            if(btn.value === "1") {
+                parrafoExpandibleUno.textContent = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo sunt eius alias itaque. Sunt, aut non? Nostrum ratione delectus, inventore recusandae praesentium perspiciatis atque quaerat et quas corrupti amet perferendis libero, eos dolores quae, sapiente quos ut nesciunt optio dolor.`
+                
+                parrafoExpandibleDos.textContent = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo sunt eius alias itaque. Sunt, aut non? Nostrum ratione delectus, inventore recusandae praesentium perspiciatis atque quaerat et quas corrupti amet perferendis libero, eos dolores quae, sapiente quos ut nesciunt optio dolor.`
+                
+                parrafoExpandibleTres.textContent = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo sunt eius alias itaque. Sunt, aut non? Nostrum ratione delectus, inventore recusandae praesentium perspiciatis atque quaerat et quas corrupti amet perferendis libero, eos dolores quae, sapiente quos ut nesciunt optio dolor.`
+
+                divBody.appendChild(parrafoExpandibleUno)
+                divBody.appendChild(parrafoExpandibleDos)
+                divBody.appendChild(parrafoExpandibleTres)
+
+            }else if(btn.value === "2") {
+                parrafoExpandibleUno.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo sunt eius alias itaque. Sunt, aut non? Nostrum ratione delectus, inventore recusandae praesentium perspiciatis atque quaerat et quas corrupti amet perferendis libero, eos dolores quae, sapiente quos ut nesciunt optio dolor."
+
+                parrafoExpandibleUno.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo sunt eius alias itaque. Sunt, aut non? Nostrum ratione delectus, inventore recusandae praesentium perspiciatis atque quaerat et quas corrupti amet perferendis libero, eos dolores quae, sapiente quos ut nesciunt optio dolor."
+
+                divBody.appendChild(parrafoExpandibleUno)
+                divBody.appendChild(parrafoExpandibleDos)
+            }else if(btn.value === "3") {
+                parrafoExpandibleUno.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo sunt eius alias itaque. Sunt, aut non? Nostrum ratione delectus, inventore recusandae praesentium perspiciatis atque quaerat et quas corrupti amet perferendis libero, eos dolores quae, sapiente quos ut nesciunt optio dolor."
+
+                divBody.appendChild(parrafoExpandibleUno)
+            } else if(btn.value === "4") {
+                parrafoExpandibleUno.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo sunt eius alias itaque. Sunt, aut non? Nostrum ratione delectus, inventore recusandae praesentium perspiciatis atque quaerat et quas corrupti amet perferendis libero, eos dolores quae, sapiente quos ut nesciunt optio dolor."
+                parrafoExpandibleDos.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo sunt eius alias itaque. Sunt, aut non? Nostrum ratione delectus, inventore recusandae praesentium perspiciatis atque quaerat et quas corrupti amet perferendis libero, eos dolores quae, sapiente quos ut nesciunt optio dolor."
+                divBody.appendChild(parrafoExpandibleUno)
+                divBody.appendChild(parrafoExpandibleDos)
+            }
+
+            divExpandible.appendChild(divBody)
+
+            historiaEmpresa.appendChild(divExpandible)
+        })
+    })
+}
+
+function limpiarHTML() {
+    while(historiaEmpresa.firstChild) {
+        historiaEmpresa.removeChild(historiaEmpresa.firstChild)
+    }
+}
