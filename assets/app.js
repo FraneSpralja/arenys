d04fb8b08c914d96403b6b3d680cc73af453fc10
FraneSpralja <spralja.frane@gmail.com>
2022-09-08 01:21:49 -0300

    Diseño Modal Productos

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,80 @@
 // VARIABLES
 const historiaEmpresa = document.querySelector('.empresa_historia--parrafos');
 const productosContainer = document.querySelector('.productos_container');
+const containerBody = document.querySelector('body > .container');
+
+const productos = [
+    {
+        tipo: "Artroscopía",
+        src: "./assets/img/icons/arthroscopy.png"
+    },
+    {
+        tipo: "Prótesis",
+        src: "./assets/img/icons/prosthesis.png"
+    },
+    {
+        tipo: "Visualización",
+        src: "./assets/img/icons/full-body.png"
+    },
+    {
+        tipo: "Post Operatorio",
+        src: "./assets/img/icons/stretcher.png"
+    },
+    {
+        tipo: "Bio Materiales",
+        src: "./assets/img/icons/medicina.png"
+    },
+    {
+        tipo: "C. Plaquetario",
+        src: "./assets/img/icons/blood-test.png"
+    }
+]
+
+const empresas = [
+    {
+        empresa: "Integra",
+        src: "https://www.grupoarenys.cl/arenysmed/wp-content/themes/arenys/images/logo_integra.png",
+        producto: {
+            id: [5]
+        }
+    },
+    {
+        empresa: "Durolane",
+        src: "https://www.grupoarenys.cl/arenysmed/wp-content/themes/arenys/images/logo_durolane.png",
+        producto: {
+            id: [5]
+        }
+    },
+    {
+        empresa: "Bioteccel",
+        src: "https://www.grupoarenys.cl/arenysmed/wp-content/themes/arenys/images/logo_bio.png",
+        producto: {
+            id: [5]
+        }
+    },
+    {
+        empresa: "Smith & Nephew",
+        src: "https://www.grupoarenys.cl/arenysmed/wp-content/themes/arenys/images/logo_sn.png",
+        producto: {
+            id: [1, 2, 6]
+        }
+    },
+    {
+        empresa: "Breg",
+        src: "https://www.grupoarenys.cl/arenysmed/wp-content/themes/arenys/images/logo_breg.png",
+        producto: {
+            id: [4]
+        }
+    },
+    {
+        empresa: "Endoret",
+        src: "https://www.grupoarenys.cl/arenysmed/wp-content/themes/arenys/images/logo_endoret.png",
+        producto: {
+            id: [6]
+        }
+    }
+]
+
 // EVENT LISTENER
 document.addEventListener('DOMContentLoaded', () => {
     mostrarHistoriaDeLaEmpresa();
@@ -68,36 +142,12 @@ function mostrarHistoriaDeLaEmpresa() {
 }
 
 function iconosEmpresasProductos() {
-    const productos = [
-        {
-            tipo: "Artroscopía",
-            src: "./assets/img/icons/arthroscopy.png"
-        },
-        {
-            tipo: "Prótesis",
-            src: "./assets/img/icons/prosthesis.png"
-        },
-        {
-            tipo: "Visualización",
-            src: "./assets/img/icons/full-body.png"
-        },
-        {
-            tipo: "Post Operatorio",
-            src: "./assets/img/icons/stretcher.png"
-        },
-        {
-            tipo: "Bio Materiales",
-            src: "./assets/img/icons/medicina.png"
-        },
-        {
-            tipo: "C. Plaquetario",
-            src: "./assets/img/icons/blood-test.png"
-        }
-    ]
 
     for(let i = 0; i < productos.length; i++) {
         const divContainer = document.createElement('div');
         divContainer.classList.add('productos_container--icono');
+        divContainer.setAttribute("id", `${i+1}_producto`)
+        divContainer.dataset.id = i+1
     
         const titleProductos = document.createElement('h3');
         titleProductos.classList.add('productos_container--title');
@@ -112,9 +162,72 @@ function iconosEmpresasProductos() {
 
         divContainer.appendChild(iconSpan);
         iconSpan.appendChild(iconProductos);
-        
+
         productosContainer.appendChild(divContainer);
     }
+
+    const producto = document.querySelectorAll('.productos_container--icono');
+
+    modalProductos(producto)
+
+}
+
+function modalProductos(producto) {
+    producto.forEach(icon => {
+        
+        icon.onclick = function() {
+            pintarModalProducto(icon)
+        }
+    })
+}
+
+function pintarModalProducto(prod) {
+
+    const containerModal = document.createElement('div');
+    containerModal.classList.add('container-modal');
+
+    const modalBody = document.createElement('div');
+    modalBody.classList.add('modalBody');
+    
+    const empresasLista = document.createElement('ul');
+    empresasLista.classList.add('empresas-lista');
+
+    const empresasItem = document.createElement('li');
+    empresasItem.classList.add('empresas-item');
+
+    const closeModal = document.createElement('a');
+    closeModal.classList.add('close-modal');
+    closeModal.textContent = "X"
+    closeModal.onclick = function() {
+        containerModal.remove()
+    }
+    
+    empresas.forEach(em => {
+        const { producto:{ id } } = em;
+        const productoId = parseInt(prod.dataset.id)
+
+        const divLista = document.createElement('div')
+        
+        const empresasItemSpan = document.createElement('span');
+        const empresasItemImg = document.createElement('img');
+
+        
+        if(id.includes(productoId)) {
+
+            empresasItemSpan.textContent = em.empresa;
+            empresasItemImg.src = em.src
+            
+            divLista.appendChild(empresasItemSpan);
+            divLista.appendChild(empresasItemImg);
+            
+            empresasItem.appendChild(divLista)
+        }
+        empresasLista.appendChild(empresasItem)
+        modalBody.appendChild(empresasLista)
+        modalBody.appendChild(closeModal)
+        containerModal.appendChild(modalBody)
+        containerBody.appendChild(containerModal)
+    }) 
 
 }
 
